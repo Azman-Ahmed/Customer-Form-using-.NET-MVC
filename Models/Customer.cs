dcf6413393af69d71e1fba14dd026b3e17985d8f
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-06-12 23:22:29 +0600

    changed Form

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,7 +5,10 @@
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
+        public string? Address { get; set; }
         public string? Contact { get; set; }
         public bool? willubuyagain { get; set; }
+
+        
     }
 }
